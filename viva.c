d05f269ad794380f9929ae17c106535214d3b329
wnsnaissante <ind3pe@gmail.com>
2024-11-21 23:21:42 +0900

    save_status updated

--- viva.c
+++ viva.c
@@ -26,6 +26,8 @@
 static char file_name[100];
 static char file_extension[10];
 
+
+static int is_saved = 0;
 static int crnt_page = 0;
 static int scr_csr_x = 0;
 static int scr_csr_y = 0;
@@ -35,7 +37,7 @@ void handle_key_(WINDOW* text_window, WINDOW* status_window, WINDOW* message_win
     int ch = getch();
     switch (ch) {
         case 17: // Ctrl-Q
-            if (strlen(gap_buffer->char_buffer)==0) {
+            if (strlen(gap_buffer->char_buffer)==0 || is_saved == 1) {
                 endwin(text_window);
                 endwin(status_window);
                 endwin(message_window);
@@ -61,13 +63,16 @@ void handle_key_(WINDOW* text_window, WINDOW* status_window, WINDOW* message_win
 			}
             break;
         case 19: // Ctrl-S
+            is_saved = 1;
             save_to_file(gap_buffer, file_name, file_extension);
             break;
         case 6: // Ctrl-F
             break;
         case KEY_UP:
+            
 		    break;
         case KEY_DOWN:
+
             break;
         case KEY_LEFT:
             scr_csr_x--;
@@ -80,6 +85,7 @@ void handle_key_(WINDOW* text_window, WINDOW* status_window, WINDOW* message_win
             break;
         case KEY_BACKSPACE:
         case 8: // Backspace
+            is_saved = 0;
             if (scr_csr_y > 0) {
                 if (scr_csr_x >= 1) {
                     delete_char(gap_buffer, gap_buffer_cursor_1d_position);
@@ -111,20 +117,23 @@ void handle_key_(WINDOW* text_window, WINDOW* status_window, WINDOW* message_win
 	    case KEY_END:
 		    break;
 	    default:
+            is_saved = 0;
             insert_char(gap_buffer, ch, gap_buffer_cursor_1d_position);
             gap_buffer_cursor_1d_position++;
             if (ch == 10) {
                 scr_csr_x = 0;
                 scr_csr_y++;
-
                 move(scr_csr_y, scr_csr_x);
             }
             else {
                 scr_csr_x++;
-
                 move(scr_csr_y, scr_csr_x);
             }
-            
+            if (scr_csr_y == LINES - 2) {
+                crnt_page++;
+                scr_csr_y-=1;
+                move(scr_csr_y, scr_csr_x);
+			}
             move(scr_csr_y, scr_csr_x);
 		    break;
         }
@@ -166,10 +175,16 @@ int main(int argc, char* argv[]) {
     while (1) {
 
         handle_key_(text_window, status_window, message_window, gap_buffer);
+        if (scr_csr_y == height) {
+			scroll(text_window);
+			scr_csr_y--;
+            move(scr_csr_y, scr_csr_x);
+            wrefresh(text_window);
+		}
         werase(text_window);
         draw_default_message_bar(message_window);
-        //draw_status_bar(COLS, file_name, file_extension, scr_csr_y, get_total_lines(gap_buffer), status_window);
-        draw_status_bar(COLS, file_name, file_extension, scr_csr_x, calc_page(&scr_csr_y), status_window);
+        draw_status_bar(COLS, file_name, file_extension, scr_csr_y, get_total_lines(gap_buffer), status_window);
+        // draw_status_bar(COLS, file_name, file_extension, scr_csr_x, calc_page(&scr_csr_y), status_window);
         waddstr(text_window, gap_buffer->char_buffer);
         refresh_screen(text_window, status_window, message_window);
 
