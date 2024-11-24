1912a52c1c58c46a030d366aaeaf21a9ff25b95e
3phemera <ind3pe@gmail.com>
2024-11-24 19:42:15 +0900

    Add functions for position calculation and save message bar
    Introduce `calc_opening_position` to compute initial cursor placement based on the GapBuffer's content. Also, add `draw_save_complete_message_bar` to notify users when a file is saved.

--- viva.c
+++ viva.c
@@ -23,16 +23,23 @@
 
 #define WIN64_KEY_HOME 449
 #define WIN64_KEY_END 455
+#define WIN64_KEY_BACKSPACE 8
+#define WIN64_KEY_CTRL_Q 17
+#define WIN64_KEY_CTRL_S 19
+#define WIN64_KEY_CTRL_F 6
+#define WIN64_KEY_PAGE_UP 451
+#define WIN64_KEY_PAGE_DOWN 457
 
 #include "terminal.h"
 #include "gap_buffer.h"
+
+static GapBuffer* filename_buffer;
 
 static char file_name[100];
 static char file_extension[10];
 
 
-static int is_saved = 0;
-static int crnt_page = 0;
+static _Bool is_saved = 0;
 static int scr_csr_x = 0;
 static int scr_csr_y = 0;
 static int gap_buffer_cursor_1d_position = 0;
@@ -44,7 +51,7 @@ void handle_key_(WINDOW* text_window, WINDOW* status_window, WINDOW* message_win
     refresh();
     int ch = getch();
     switch (ch) {
-    case 17: // Ctrl-Q
+    case WIN64_KEY_CTRL_Q:
         if (strlen(gap_buffer->char_buffer) == 0 || is_saved == 1) {
             exit(0);
             break;
@@ -64,11 +71,42 @@ void handle_key_(WINDOW* text_window, WINDOW* status_window, WINDOW* message_win
             }
         }
         break;
-    case 19: // Ctrl-S
+    case WIN64_KEY_CTRL_S: // Ctrl-S
+        if (strlen(file_name) == 0 && strlen(file_extension) == 0) {
+            werase(message_window);
+            waddstr(message_window, "Type filename.extension");
+            wrefresh(message_window);
+            char input_char;
+            int position = 0;
+            do {
+                input_char = getch();
+                if (input_char != '\n') {
+                    if (input_char == KEY_BACKSPACE || input_char == WIN64_KEY_BACKSPACE) {
+                        delete_char(filename_buffer, position);
+                        position--;
+                    }
+                    insert_char(filename_buffer, input_char, position++);
+                    werase(message_window);
+                    waddstr(message_window, filename_buffer->char_buffer);
+                    wrefresh(message_window);
+                }
+            } while (input_char != '\n');
+            if (strpbrk(filename_buffer->char_buffer, ".") != NULL) {
+                strcpy(file_extension, strpbrk(filename_buffer->char_buffer, ".")); // get file extension
+                strcpy(file_name, strtok(filename_buffer->char_buffer, ".")); // get file name
+            }
+            else {
+                strcpy(file_name, filename_buffer->char_buffer);
+                strcpy(file_extension, "\0");
+            }
+        }
         is_saved = 1;
         save_to_file(gap_buffer, file_name, file_extension);
+        werase(message_window);
+        draw_save_complete_message_bar(message_window);
+        wrefresh(message_window);
         break;
-    case 6: // Ctrl-F
+    case WIN64_KEY_CTRL_F: // Ctrl-F
         break;
     case KEY_UP:
 
@@ -77,24 +115,33 @@ void handle_key_(WINDOW* text_window, WINDOW* status_window, WINDOW* message_win
 
         break;
     case KEY_LEFT:
-        scr_csr_x--;
-        gap_buffer_cursor_1d_position--;
+        if (scr_csr_x > 0) {
+            scr_csr_x--;
+            gap_buffer_cursor_1d_position--;
+        } else {
+            if (scr_csr_x == 0 && scr_csr_y == 0) {
+                break;
+            }
+            calc_last_line_change_position(gap_buffer, gap_buffer_cursor_1d_position, &scr_csr_x, &scr_csr_y);
+            if (scr_csr_y > LINES - 2) {
+                scr_csr_y = LINES - 2;
+                move(LINES - 1, scr_csr_y);
+                refresh();
+            }
+            move(scr_csr_y, scr_csr_x);
+            gap_buffer_cursor_1d_position--;
+            if (gap_buffer->char_buffer[gap_buffer_cursor_1d_position] != '\n') {
+                scr_csr_y--;
+                move(scr_csr_y, scr_csr_x);
+            }
+        }
         move(scr_csr_y, scr_csr_x);
         break;
     case KEY_RIGHT:
-        if (gap_buffer->char_buffer[gap_buffer_cursor_1d_position] == '\n') {
-            scr_csr_x = 0;
-            scr_csr_y++;
-        }
-        else {
-            scr_csr_x++;
-        }
-        gap_buffer_cursor_1d_position++;
 
-        move(scr_csr_y, scr_csr_x);
         break;
     case KEY_BACKSPACE:
-    case 8: // Backspace
+    case WIN64_KEY_BACKSPACE:
         if (gap_buffer->char_buffer[gap_buffer_cursor_1d_position - 1] == '\n') {
             total_lines--;
         }
@@ -138,6 +185,19 @@ void handle_key_(WINDOW* text_window, WINDOW* status_window, WINDOW* message_win
     case WIN64_KEY_END:
     case KEY_END:
         break;
+    case KEY_NPAGE:
+    case WIN64_KEY_PAGE_DOWN:
+        wscrl(text_window, LINES - 2);
+        wrefresh(text_window);
+        wrefresh(text_window);
+        wmove(text_window, LINES - 2, 0);
+        break;
+    case KEY_PPAGE:
+    case WIN64_KEY_PAGE_UP:
+        wscrl(text_window, -(LINES - 2));
+        wrefresh(text_window);
+        wmove(text_window, LINES - 2, 0);
+        break;
     default:
         is_saved = 0;
         insert_char(gap_buffer, ch, gap_buffer_cursor_1d_position);
@@ -169,8 +229,7 @@ void handle_key_(WINDOW* text_window, WINDOW* status_window, WINDOW* message_win
             move(scr_csr_y, scr_csr_x);
         }
         if (scr_csr_y == LINES - 2) {
-            crnt_page++;
-            scr_csr_y -= 1;
+            scr_csr_y--;
             move(scr_csr_y, scr_csr_x);
         }
         move(scr_csr_y, scr_csr_x);
@@ -184,6 +243,7 @@ int main(int argc, char* argv[]) {
     noecho();
     keypad(stdscr, TRUE);
 
+    filename_buffer = create_gap_buffer(FILENAME_MAX);
 
     GapBuffer* gap_buffer = create_gap_buffer(1024);
     if (argc > 1)   // get file name and its extension from parameter
@@ -197,10 +257,10 @@ int main(int argc, char* argv[]) {
 			strcpy(file_name, original_file_name);
 			strcpy(file_extension, "\0");
         }
-
-        open_file(gap_buffer, file_name, file_extension);
-        gap_buffer_cursor_1d_position = 0;
+        gap_buffer_cursor_1d_position = open_file(gap_buffer, file_name, file_extension);
+        calc_opening_position(gap_buffer, &scr_csr_x, &scr_csr_y, gap_buffer_cursor_1d_position);
         total_lines = get_total_lines(gap_buffer);
+        free(filename_buffer);
     }
     else {
         strcpy(file_name, "\0");
@@ -208,31 +268,35 @@ int main(int argc, char* argv[]) {
     }
 
 
-    int height = LINES - 2;
-    int width = COLS;
-
     // Text Area
-    WINDOW* text_window = newwin(height, width, 0, 0);
+    WINDOW* text_window = newwin(LINES - 2, COLS, 0, 0);
 
     // Status Area
-    WINDOW* status_window = newwin(1, width, height, 0);
+    WINDOW* status_window = newwin(1, COLS, LINES - 2, 0);
 
     // Message Area
-    WINDOW* message_window = newwin(1, width, height + 1, 0);
+    WINDOW* message_window = newwin(1, COLS, LINES - 2 + 1, 0);
     scrollok(text_window, TRUE);
 
     while (1) {
+        if (scr_csr_y > LINES - 2) {
+            while (scr_csr_y < LINES - 2) {
+                scr_csr_y--;
+                wmove(text_window, LINES - 2, scr_csr_x);
+                wrefresh(text_window);
+            }
+        }
         handle_key_(text_window, status_window, message_window, gap_buffer);
-        if (scr_csr_y == height) {
+        if (scr_csr_y == LINES - 2) {
             scroll(text_window);
             scr_csr_y--;
             move(scr_csr_y, scr_csr_x);
             wrefresh(text_window);
         }
+
         werase(text_window);
-        draw_default_message_bar(message_window);
         draw_status_bar(COLS, file_name, file_extension, scr_csr_y, total_lines, status_window);
-        // draw_status_bar(COLS, file_name, file_extension, scr_csr_x, scr_csr_y, status_window);
+        //draw_status_bar(COLS, file_name, file_extension, scr_csr_x, scr_csr_y, status_window);
         draw_text_area(text_window, gap_buffer);
         refresh_screen(text_window, status_window, message_window);
     }
