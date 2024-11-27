08f9e6f1655ff6eec3bb16faa9102db262efdf2e
3phemera <ind3pe@gmail.com>
2024-11-27 17:58:36 +0900

    Add new key definitions and remove redundant cursor functions
    Added several key definitions for both Windows and Mac environments to `terminal.h`. Removed multiple cursor-related function prototypes to simplify the header file and replaced them with a single `find_word_position` function. This streamlines cursor management and improves code maintainability.

--- terminal.h
+++ terminal.h
@@ -1,6 +1,19 @@
 #ifndef TERMINAL_H
 #define TERMINAL_H
 
+
+
+#define WIN64_KEY_HOME 449
+#define WIN64_KEY_END 455
+#define WIN64_KEY_BACKSPACE 8
+#define KEY_CTRL_Q 17
+#define KEY_CTRL_S 19
+#define KEY_CTRL_F 6
+#define WIN64_KEY_PAGE_UP 451
+#define WIN64_KEY_PAGE_DOWN 457
+#define MAC_BACKSPACE 127
+#define MAC_CTRL_F 6
+
 #include <stdio.h>
 #include "gap_buffer.h"
 #include <curses.h>
@@ -10,12 +23,8 @@ void draw_status_bar(int width, char* file_name, char* file_extension, int curre
 void draw_default_message_bar(WINDOW* message_window);
 void refresh_screen(WINDOW* text_window, WINDOW* status_window, WINDOW* message_window);
 void draw_quit_message_bar(WINDOW* message_window);
-void move_cursor(WINDOW* text_window, int* scr_csr_x, int* scr_csr_y);
-void calc_opening_position(const GapBuffer* gap_buffer, int* scr_csr_x, int* scr_csr_y, int gap_start);
 void draw_save_complete_message_bar(WINDOW* message_window);
-void calc_cursor_position(GapBuffer* gap_buffer, int* gap_buffer_cursor_position, int* scr_csr_x, int* scr_csr_y);
-void calibration_cursor_position(GapBuffer* gap_buffer, int* base_scr_csr_x, int* base_scr_csr_y, int* relative_x, int* relative_y, int* scroll_offset);
 int calc_render_start_index(GapBuffer* gap_buffer, int current_cursor_row);
 void calculate_screen_1dim_pos(GapBuffer* gap_buffer, int base_1dim_pos, int crnt_screen_cursor_rel_1dim_pos, int* calculated_screen_cursor_1dim_pos);
-
+void find_word_position(GapBuffer* gap_buffer, GapBuffer* word_buffer, int* gap_buffer_cursor_position, WINDOW* message_window, WINDOW* status_window);
 #endif // TERMINAL_H
